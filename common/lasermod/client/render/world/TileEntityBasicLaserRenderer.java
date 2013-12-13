b11210c19f2588b652c214077d64e6e0a338cd51
ProPercivalalb <propercivalalb@gmail.com>
2013-12-13 06:35:04 +0000

    Working on laser render

--- common/lasermod/client/render/world/TileEntityBasicLaserRenderer.java
+++ common/lasermod/client/render/world/TileEntityBasicLaserRenderer.java
@@ -1,8 +1,13 @@
 package lasermod.client.render.world;
 
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+
 import lasermod.tileentity.TileEntityBasicLaser;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 
 /**
  * @author ProPercivalalb
@@ -10,8 +15,86 @@ import net.minecraft.tileentity.TileEntity;
 public class TileEntityBasicLaserRenderer extends TileEntitySpecialRenderer {
 
     public void renderBasicLaser(TileEntityBasicLaser basicLaser, double x, double y, double z, float tick) {
-    	
+    	//FMLClientHandler.instance().getClient().func_110434_K().func_110577_a(Properties.RES_BLOCK_MAGNETIC_CHEST);
+        GL11.glPushMatrix();
+        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        GL11.glTranslatef((float) x, (float) y + 1.0F, (float) z + 1.0F);
+        GL11.glScalef(1.0F, -1.0F, -1.0F);
+        GL11.glTranslatef(0.5F, 0.5F, 0.5F);
+        
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 0.7F);
+        AxisAlignedBB laser = basicLaser.getLaserBox();
+        this.drawBoundingBox(laser);
+        
+        //GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+        GL11.glPopMatrix();
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
     }
+    
+    public static void drawBoundingBox(AxisAlignedBB boundingBox) {
+	    Tessellator tessellator = Tessellator.instance;
+	    tessellator.startDrawingQuads();
+	    tessellator.addVertex(boundingBox.minX, boundingBox.minY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.maxY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.minY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.maxY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.minY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.maxY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.minY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.maxY, boundingBox.maxZ);
+	    tessellator.draw();
+	    tessellator.startDrawingQuads();
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.maxY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.minY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.maxY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.minY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.maxY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.minY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.maxY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.minY, boundingBox.maxZ);
+	    tessellator.draw();
+	    tessellator.startDrawingQuads();
+	    tessellator.addVertex(boundingBox.minX, boundingBox.maxY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.maxY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.maxY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.maxY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.maxY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.maxY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.maxY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.maxY, boundingBox.minZ);
+	    tessellator.draw();
+	    tessellator.startDrawingQuads();
+	    tessellator.addVertex(boundingBox.minX, boundingBox.minY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.minY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.minY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.minY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.minY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.minY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.minY, boundingBox.maxZ);
+	    tessellator.draw();
+	    tessellator.startDrawingQuads();
+	    tessellator.addVertex(boundingBox.minX, boundingBox.minY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.maxY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.minY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.maxY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.minY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.maxY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.minY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.maxY, boundingBox.minZ);
+	    tessellator.draw();
+	    tessellator.startDrawingQuads();
+	    tessellator.addVertex(boundingBox.minX, boundingBox.maxY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.minY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.maxY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.minX, boundingBox.minY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.maxY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.minY, boundingBox.minZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.maxY, boundingBox.maxZ);
+	    tessellator.addVertex(boundingBox.maxX, boundingBox.minY, boundingBox.maxZ);
+	    tessellator.draw();
+	}
 
     @Override
     public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float tick) {
--- common/lasermod/tileentity/TileEntityBasicLaser.java
+++ common/lasermod/tileentity/TileEntityBasicLaser.java
@@ -1,10 +1,52 @@
 package lasermod.tileentity;
 
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 
 /**
  * @author ProPercivalalb
  */
 public class TileEntityBasicLaser extends TileEntity {
 
+	public AxisAlignedBB getLaserBox() {
+		int i = getBlockMetadata();
+		double laserSize = 0.4D;
+		AxisAlignedBB boundingBox = AxisAlignedBB.getBoundingBox(this.xCoord + 0.5D - laserSize / 2, this.yCoord + 0.5D - laserSize / 2, this.zCoord + 0.5D - laserSize / 2, this.xCoord + 0.5D + laserSize / 2, this.yCoord + 0.5D + laserSize / 2, this.zCoord + 0.5D + laserSize / 2);
+
+        if (i == 0)
+        {
+        	boundingBox.expand(2.0D, 2.0D, 2.0D);
+        }
+
+        if (i == 1)
+        {
+            //return AxisAlignedBB.getBoundingBox(xCoord, yCoord, zCoord, (double)xCoord + 1.0D, (double)yCoord + 1.0D + d, (double)zCoord + 1.0D);
+        }
+
+        if (i == 2)
+        {
+            //return AxisAlignedBB.getBoundingBox(xCoord, yCoord, (double)zCoord - d, (double)xCoord + 1.0D, (double)yCoord + 1.0D, (double)zCoord + 1.0D);
+        }
+
+        if (i == 3)
+        {
+            //return AxisAlignedBB.getBoundingBox(xCoord, yCoord, zCoord, (double)xCoord + 1.0D, (double)yCoord + 1.0D, (double)zCoord + 1.0D + d);
+        }
+
+        if (i == 4)
+        {
+            //return AxisAlignedBB.getBoundingBox((double)xCoord - d, yCoord, zCoord, (double)xCoord + 1.0D, (double)yCoord + 1.0D, (double)zCoord + 1.0D);
+        }
+
+        if (i == 5)
+        {
+            //return AxisAlignedBB.getBoundingBox(xCoord, yCoord, zCoord, (double)xCoord + 1.0D + d, (double)yCoord + 1.0D, (double)zCoord + 1.0D);
+        }
+        else
+        {
+            //return AxisAlignedBB.getBoundingBox(xCoord, yCoord, zCoord, (double)xCoord + 1.0D, (double)yCoord + 1.0D, (double)zCoord + 1.0D);
+        }
+        return boundingBox;
+	}
+	
 }
