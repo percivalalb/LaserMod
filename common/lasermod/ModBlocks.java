2324668214d530470cbb9ea56a72183527969845
ProPercivalalb <propercivalalb@gmail.com>
2013-12-13 06:19:00 +0000

    Placement and temp textures in place

--- common/lasermod/ModBlocks.java
+++ common/lasermod/ModBlocks.java
@@ -2,6 +2,8 @@ package lasermod;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.common.MinecraftForge;
 import lasermod.block.BlockBasicLaser;
 import lasermod.lib.BlockIds;
 
@@ -13,8 +15,10 @@ public class ModBlocks {
 	public static Block basicLaser;
 	
 	public static void inti() {
-		basicLaser = new BlockBasicLaser(BlockIds.ID_BASIC_LASER).setHardness(1.0F);
+		basicLaser = new BlockBasicLaser(BlockIds.ID_BASIC_LASER).setHardness(1.0F).setCreativeTab(CreativeTabs.tabBlock);
 		
 		GameRegistry.registerBlock(basicLaser, "lasermod.basicLaser");
+		
+		MinecraftForge.setBlockHarvestLevel(basicLaser, "pickaxe", 1);
 	}
 }
--- common/lasermod/block/BlockBasicLaser.java
+++ common/lasermod/block/BlockBasicLaser.java
@@ -1,9 +1,15 @@
 package lasermod.block;
 
 import lasermod.tileentity.TileEntityBasicLaser;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Facing;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 
 /**
@@ -20,4 +26,40 @@ public class BlockBasicLaser extends BlockContainer {
 		return new TileEntityBasicLaser();
 	}
 
+	public static int getOrientation(int par1) {
+		return par1 & 7;
+	}
+	    
+	@Override
+	public Icon getIcon(int par1, int par2) {
+	    int meta = getOrientation(par2);
+
+	    if (meta > 5)
+	        return this.blockIcon;
+
+	    if (par1 == meta)
+	    {
+	        return this.blockIcon;
+	    }
+	    else
+	    {
+	    	return par1 == Facing.oppositeSide[meta] ? Block.planks.getIcon(2, 2) : Block.planks.getIcon(0, 1);
+	    }
+	}
+
+	@Override
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack stack) {
+	    int rotation = 0;
+	        
+	    if (Math.abs(player.rotationPitch) > 90D / 2D)
+	        if (player.rotationPitch > 0)
+	            rotation = 1;
+	        else if (player.rotationPitch < 0)
+	            rotation = 0;
+	        else
+	            rotation = Direction.directionToFacing[Direction.rotateOpposite[Math.round(player.rotationYaw / 90) & 3]];
+	        
+	        
+	        world.setBlockMetadataWithNotify(x, y, z, rotation, 2);
+	    }
 }
