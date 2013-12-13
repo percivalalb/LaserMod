83e6157275e7f76b7ad41113ce7ed1243ca74826
ProPercivalalb <propercivalalb@gmail.com>
2013-12-13 06:23:35 +0000

    Fixed placement

--- common/lasermod/block/BlockBasicLaser.java
+++ common/lasermod/block/BlockBasicLaser.java
@@ -10,6 +10,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Facing;
 import net.minecraft.util.Icon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 /**
@@ -48,18 +49,29 @@ public class BlockBasicLaser extends BlockContainer {
 	}
 
 	@Override
-	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack stack) {
-	    int rotation = 0;
-	        
-	    if (Math.abs(player.rotationPitch) > 90D / 2D)
-	        if (player.rotationPitch > 0)
-	            rotation = 1;
-	        else if (player.rotationPitch < 0)
-	            rotation = 0;
-	        else
-	            rotation = Direction.directionToFacing[Direction.rotateOpposite[Math.round(player.rotationYaw / 90) & 3]];
-	        
-	        
-	        world.setBlockMetadataWithNotify(x, y, z, rotation, 2);
-	    }
+	public void onBlockPlacedBy(World par1World, int x, int y, int z, EntityLivingBase par5EntityLiving, ItemStack par6ItemStack) {
+		 int rotation = determineOrientation(par1World, x, y, z, par5EntityLiving);
+		 par1World.setBlockMetadataWithNotify(x, y, z, rotation, 2);
+	}
+	
+	public static int determineOrientation(World par0World, int par1, int par2, int par3, EntityLivingBase par4EntityLivingBase)
+    {
+        if (MathHelper.abs((float)par4EntityLivingBase.posX - (float)par1) < 2.0F && MathHelper.abs((float)par4EntityLivingBase.posZ - (float)par3) < 2.0F)
+        {
+            double d0 = par4EntityLivingBase.posY + 1.82D - (double)par4EntityLivingBase.yOffset;
+
+            if (d0 - (double)par2 > 2.0D)
+            {
+                return 1;
+            }
+
+            if ((double)par2 - d0 > 0.0D)
+            {
+                return 0;
+            }
+        }
+
+        int l = MathHelper.floor_double((double)(par4EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+        return l == 0 ? 2 : (l == 1 ? 5 : (l == 2 ? 3 : (l == 3 ? 4 : 0)));
+    }
 }
