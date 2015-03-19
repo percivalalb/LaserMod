471f2b8cc9dd5a5428f83b414a6d98a6a7ed5fb2
Émile Bolduc <therealwebmilio@gmail.com>
2015-03-18 23:03:30 -0400

    Last commit for the day.

--- src/main/java/lasermod/laser/GrowthLaser.java
+++ src/main/java/lasermod/laser/GrowthLaser.java
@@ -3,35 +3,38 @@ package lasermod.laser;
 import java.util.List;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.monster.EntitySlime;
 import net.minecraft.entity.player.EntityPlayer;
 import lasermod.api.ILaser;
 
+/**
+ * @author webmilio
+ */
 public class GrowthLaser implements ILaser {
 
 	@Override
 	public void performActionOnEntitiesBoth(List<Entity> entities, int direction) {
-		// TODO Auto-generated method stub
+		for (Entity entity : entities)
+		{
+			
+		}
+	}
+
+	@Override
+	public void performActionOnEntitiesClient(List<Entity> entities, int direction) {
 		
 	}
 
 	@Override
-	public void performActionOnEntitiesClient(List<Entity> entities,
-			int direction) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void performActionOnEntitiesServer(List<Entity> entities,
-			int direction) {
-		// TODO Auto-generated method stub
+	public void performActionOnEntitiesServer(List<Entity> entities, int direction) {
 		
 	}
 
 	@Override
 	public boolean shouldRenderLaser(EntityPlayer player, int direction) {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 	
 }
