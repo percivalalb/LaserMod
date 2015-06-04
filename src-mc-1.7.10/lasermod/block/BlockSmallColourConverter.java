580fe5aa1731360bb80a46a17dc240dac3646974
ProPercivalalb <propercivalalb@gmail.com>
2015-06-04 20:37:06 +0100

    Clean up in BlockSmallColourConverter.java

--- src-mc-1.7.10/lasermod/block/BlockSmallColourConverter.java
+++ src-mc-1.7.10/lasermod/block/BlockSmallColourConverter.java
@@ -21,6 +21,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.Facing;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
@@ -111,13 +112,13 @@ public class BlockSmallColourConverter extends BlockContainer {
     }
 
 	@Override
-    public boolean canPlaceBlockAt(World p_149742_1_, int p_149742_2_, int p_149742_3_, int p_149742_4_) {
-        return (p_149742_1_.isSideSolid(p_149742_2_ - 1, p_149742_3_, p_149742_4_, EAST)) ||
-               (p_149742_1_.isSideSolid(p_149742_2_ + 1, p_149742_3_, p_149742_4_, WEST)) ||
-               (p_149742_1_.isSideSolid(p_149742_2_, p_149742_3_, p_149742_4_ - 1, SOUTH)) ||
-               (p_149742_1_.isSideSolid(p_149742_2_, p_149742_3_, p_149742_4_ + 1, NORTH)) ||
-               (p_149742_1_.isSideSolid(p_149742_2_, p_149742_3_ - 1, p_149742_4_, UP)) ||
-               (p_149742_1_.isSideSolid(p_149742_2_, p_149742_3_ + 1, p_149742_4_, DOWN));
+    public boolean canPlaceBlockAt(World world, int x, int y, int z) {
+        return (world.isSideSolid(x - 1, y, z, EAST)) ||
+               (world.isSideSolid(x + 1, y, z, WEST)) ||
+               (world.isSideSolid(x, y, z - 1, SOUTH)) ||
+               (world.isSideSolid(x, y, z + 1, NORTH)) ||
+               (world.isSideSolid(x, y - 1, z, UP)) ||
+               (world.isSideSolid(x, y + 1, z, DOWN));
     }
 	
 	@Override
@@ -126,21 +127,18 @@ public class BlockSmallColourConverter extends BlockContainer {
     }
 	
 	@Override
-	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float xHit, float yHit, float zHit) {
-		ItemStack item = player.getCurrentEquippedItem();
-		if(!world.isRemote && item != null) {
-			if(item.getItem() == Items.dye) {
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+		ItemStack stack = player.getCurrentEquippedItem();
+		if(!world.isRemote && stack != null) {
+			if(stack.getItem() == Items.dye) {
 				TileEntitySmallColourConverter colourConverter = (TileEntitySmallColourConverter)world.getTileEntity(x, y, z);
 				
-				int colour = 15 - item.getItemDamage();
-				if(colour > 15) colour = 15;
-				else if(colour < 0) colour = 0;
+				int colour = MathHelper.clamp_int(15 - stack.getItemDamage(), 0, 15);
 				
 				if(colour == colourConverter.colour) return true;
 				
 				colourConverter.colour = colour;
-				if(!player.capabilities.isCreativeMode) item.stackSize--;
-				if(item.stackSize <= 0) player.setCurrentItemOrArmor(0, (ItemStack)null);
+	      		if(!player.capabilities.isCreativeMode && --stack.stackSize <= 0) player.inventory.setInventorySlotContents(player.inventory.currentItem, (ItemStack)null);
 				
 				PacketDispatcher.sendToAllAround(new SmallColourConverterMessage(colourConverter), colourConverter, 512);
 				return true;
@@ -156,5 +154,5 @@ public class BlockSmallColourConverter extends BlockContainer {
 	public boolean renderAsNormalBlock() { return false; }
 
 	@Override
-    public int onBlockPlaced(World world, int x, int y, int z, int side, float p_149660_6_, float p_149660_7_, float p_149660_8_, int meta) { return side; }
+    public int onBlockPlaced(World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ, int meta) { return side; }
 }
