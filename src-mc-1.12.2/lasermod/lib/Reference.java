3542b3f5feda0cc6c85ea665333466fde33414f1
ProPercivalalb <propercivalalb@gmail.com>
2019-05-01 18:02:15 +0100

    Require latest forge version for 1.12

--- src-mc-1.12.2/lasermod/lib/Reference.java
+++ src-mc-1.12.2/lasermod/lib/Reference.java
@@ -9,10 +9,8 @@ public class Reference {
 	public static final String		MOD_ID			= "lasermod";
 	public static final String		MOD_NAME		= "LaserMod";
 	public static final String		MOD_VERSION		= "v1.2.0a";
-	public static final String		MOD_DEPENDENCIES= "required-after:Forge@[9.10.1.850,)";
+	public static final String		MOD_DEPENDENCIES= "required-after:Forge@[14.23.5.2768,)";
 	public static final String		SP_CLIENT		= "lasermod.proxy.ClientProxy";
 	public static final String		SP_SERVER		= "lasermod.proxy.CommonProxy";
 	public static final String      CHANNEL_NAME	= "LASER";
-	
-	public static final boolean		DEBUG			= false;
 }
