a6c5291a9f226e4aa86e1a9a097d7ecb743560e9
ProPercivalalb <propercivalalb@gmail.com>
2019-05-01 08:42:53 +0100

    Delete old recipe code

--- src-mc-1.12.2/lasermod/RegistryHandling.java
+++ src-mc-1.12.2/lasermod/RegistryHandling.java
@@ -31,29 +31,6 @@ public class RegistryHandling {
 		registerLaser("damage", new DamageLaser(), 7);
 		registerLaser("healing", new HealingLaser(), 8);
 		
-		//TODO Add recipes
-		/**
-		GameRegistry.addRecipe(new ItemStack(ModItems.LASER_CRYSTAL), " r ", "rgr", " r ", 'g', Blocks.glass, 'r', Items.REDSTONE);
-		GameRegistry.addRecipe(new ItemStack(ModItems.SCREWDRIVER, 1), new Object[] {"  I", "YB ", "BY ", 'B', new ItemStack(Items.DYE, 1, 0), 'Y', new ItemStack(Items.DYE, 1, 11), 'I', Items.IRON_INGOT});
-		GameRegistry.addRecipe(new ItemStack(ModItems.SCREWDRIVER, 1), new Object[] {"  I", "BY ", "YB ", 'B', new ItemStack(Items.DYE, 1, 0), 'Y', new ItemStack(Items.DYE, 1, 11), 'I', Items.IRON_INGOT});
-		GameRegistry.addRecipe(new ItemStack(ModItems.LASER_SEEKING_GOOGLES, 1), new Object[] {"I I", "I I", "GLG", 'G', Blocks.glass, 'L', ModItems.LASER_CRYSTAL, 'I', Items.IRON_INGOT});
-		GameRegistry.addRecipe(new ItemStack(ModBlocks.LASER_ADVANCED, 1), new Object[] {"OGO", "SLS", "OGO", 'G', Blocks.glass, 'L', ModItems.LASER_CRYSTAL, 'S', Items.IRON_INGOT, 'O', Blocks.obsidian});
-		GameRegistry.addRecipe(new ItemStack(ModBlocks.LASER_BASIC, 1), new Object[] {"CGC", "GLG", "CGC", 'G', Blocks.glass, 'L', ModItems.LASER_CRYSTAL, 'C', Blocks.cobblestone});
-		GameRegistry.addRecipe(new ItemStack(ModBlocks.REFLECTOR, 1), new Object[] {"CGC", "GSG", "CGC", 'G', Blocks.glass, 'C', Blocks.cobblestone, 'S', Items.glowstone_dust});
-		GameRegistry.addRecipe(new ItemStack(ModBlocks.COLOUR_CONVERTER, 1), new Object[] {"CGC", "GLG", "CGC", 'G', Items.glowstone_dust, 'L', ModItems.LASER_CRYSTAL, 'C', Blocks.cobblestone});
-		GameRegistry.addRecipe(new ItemStack(ModBlocks.LASER_DETECTOR, 1), new Object[] {"SSS", "SBS", "SSS", 'B', Blocks.redstone_block, 'S', Blocks.stone});
-		GameRegistry.addRecipe(new ItemStack(ModItems.HANDHELD_SENSOR, 1), new Object[] {" T ", "ICI", " I ", 'T', Blocks.redstone_torch, 'I', Items.IRON_INGOT, 'C', ModItems.LASER_CRYSTAL});
-		GameRegistry.addRecipe(new ItemStack(ModBlocks.LUMINOUS_LAMP, 1), new Object[] {" G ", "GDG", " I ", 'G', Blocks.glass, 'D', Items.glowstone_dust});
-				
-		//GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colourConverter, 1, 0), new Object[] {ModBlocks.smallColourConverter});    
-		//GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.smallColourConverter, 1, 0), new Object[] {ModBlocks.colourConverter});
-		
-		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.UPGRADES, 1, 0), new Object[] {Blocks.obsidian, Items.fire_charge, Items.blaze_powder, Items.glowstone_dust});
-		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.UPGRADES, 1, 3), new Object[] {Blocks.obsidian, Items.golden_carrot, Items.fermented_spider_eye, Items.glowstone_dust});
-		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.UPGRADES, 1, 5), new Object[] {Blocks.obsidian, Blocks.piston, Items.feather, Items.glowstone_dust});
-		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.UPGRADES, 1, 6), new Object[] {Blocks.obsidian, Blocks.sticky_piston, Items.feather, Items.glowstone_dust});
-		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.UPGRADES, 1, 7), new Object[] {Blocks.obsidian, Items.golden_sword, Items.SPIDER_EYE, Items.glowstone_dust});
-		**/
 		// List of blocks lasers can go through.
 		Block[] blocksWhitelistPassThrough = new Block[] { Blocks.AIR, Blocks.ICE, Blocks.TALLGRASS, Blocks.DOUBLE_PLANT, Blocks.LEAVES, 
 				Blocks.LEAVES2, Blocks.LEVER, Blocks.TORCH, Blocks.LADDER, Blocks.REDSTONE_TORCH, Blocks.UNLIT_REDSTONE_TORCH, 
