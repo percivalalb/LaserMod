a0957025a73fd31f8cc296ac4f1f6720256368e5
ProPercivalalb <propercivalalb@gmail.com>
2019-05-01 18:04:56 +0100

    Fix COLOUR_CONVERTER_SMALL id

--- src-mc-1.12.2/lasermod/lib/BlockNames.java
+++ src-mc-1.12.2/lasermod/lib/BlockNames.java
@@ -7,7 +7,7 @@ public class BlockNames {
 	public static final String LASER_DETECTOR = Reference.MOD_ID + ":detector";
 	public static final String REFLECTOR = Reference.MOD_ID + ":reflector";
 	public static final String COLOUR_CONVERTER = Reference.MOD_ID + ":colour_converter";
-	public static final String COLOUR_CONVERTER_SMALL = Reference.MOD_ID + ":small_colour_converter";
+	public static final String COLOUR_CONVERTER_SMALL = Reference.MOD_ID + ":colour_converter_small";
 	public static final String LUMINOUS_LAMP = Reference.MOD_ID + ":luminous_lamp";
 	public static final String LENS_WORKBENCH = Reference.MOD_ID + ":lens_workbench";
 
